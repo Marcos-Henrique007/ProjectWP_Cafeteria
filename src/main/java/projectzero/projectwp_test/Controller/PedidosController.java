f327819561a8f3992b8db6fc774ef8b2b5be4932
Marcos-Henrique710 <marcoshenriqueufrn@gmail.com>
2026-05-05 11:14:49 -0300

    ajeitando PedidosController, adicionando uma nova dependência para o spring e deletando os filtros ultrapassados.

--- src/main/java/projectzero/projectwp_test/Controller/PedidosController.java
+++ src/main/java/projectzero/projectwp_test/Controller/PedidosController.java
@@ -1,6 +1,6 @@
 package projectzero.projectwp_test.Controller;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PedidosController {
 
     @GetMapping("/pedidos")
-    public String pedidos() {
-        return "pedidos";
-    }
-
-    /*
-    @GetMapping("/pedidos")
     public String pedidos(HttpSession sessao, Model model) {
 
         String usuario = (String) sessao.getAttribute("usuarioLogado");
 
-        model.addText(String body);
+        model.addAttribute("usuario", usuario);
 
         return "pedidos";
     }
-    */
 }
